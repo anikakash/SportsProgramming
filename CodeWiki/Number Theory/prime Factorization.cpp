fd84b436c07220293293ccbfb56b469eb583fe06
anikaksh <anik15-14210@diu.edu.bd>
2021-05-22 17:52:53 +0600

    Testing Sector

---
+++ CodeWiki/Number Theory/prime Factorization.cpp
@@ -0,0 +1,18 @@
+//Need to Generate prime numbers
+
+vector<int>prime_factor;
+void primeFactorization(int n)
+{
+      for(int i=0; prime[i]*prime[i]<=n; i++)
+      {
+          if(n%prime[i]==0)
+          {
+              while(n%prime[i]==0)
+              {
+                  n/=prime[i];
+                  prime_factor.pb(prime[i]); 
+              }
+          }
+      }
+    if(n>1)prime_factor.pb(n);
+}
