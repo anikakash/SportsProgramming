81ce4a7374326663ecf36a1b4bdd96edb617304e
anikaksh <anik15-14210@diu.edu.bd>
2021-05-23 14:51:18 +0600

    debug

--- Template/Help.cpp
+++ Template/Help.cpp
@@ -7,8 +7,8 @@ using namespace    std;
 #define pi           acos(-1.0) //3.1415926535897932384626
 #define dpoint(x)    fixed<<setprecision(x)
 #define debug(x)     cout<<x<<endl;
-#define gcd(a,b)     __gcd(a,b);
-#define lcm(x,y)     (a * (b / gcd(a, b)));
+// #define gcd(a,b)     __gcd(a,b);
+// #define lcm(x,y)     (a * (b / gcd(a, b)));
 #define pb           push_back
 #define mx           10000007
 
@@ -18,6 +18,9 @@ typedef unsigned long long  ul;
 
 // --------------------------Pre made Functions & Proto Type--------------------------//
 template <class T> T digitsum(T n){T sum=0;while(n!=0){sum+=n%10;n/=10;}return sum;}
+ul gcd(ul a, ul b){ ul x ; return x = __gcd(a, b);}
+ul lcm(ul a, ul b){ul y; return y = ((a)*((b)/gcd(a,b)));}
+
 int main()
 {
     #ifdef EXTRA_8
@@ -25,8 +28,11 @@ int main()
         freopen("input.txt","r",stdin);
         freopen("ans.txt","w",stdout);
     #endif
-        for(ll i=1; i<=100; i++)
-            cout<<i<<endl;
+        ul x = lcm(1,2);
+        for(ul i=3; i<=256; i++)
+            x = lcm(x,i);
+
+            cout<<x<<endl;
     #ifdef EXTRA_8
         fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
     #endif
--- Template/Main.cpp
+++ Template/Main.cpp
@@ -34,41 +34,41 @@ template <class T> T digitsum(T n){T sum=0;while(n!=0){sum+=n%10;n/=10;}return s
 int gcd(int a, int b){ int x ; return x = __gcd(a, b);}
 int lcm(int a, int b){int y; return y = ((a)*((b)/gcd(a,b)));}
 
-  vector<int>prime;
-  bool vis[mx];
-    void sieve()
-    {
-        for(int i=3; i*i<=mx; i+=2)
-        {
-            if(vis[i]==0)
-            {
-                for(int j = i*i; j<mx; j+=2*i)
-                  vis[j]=1;
-            }
-        }
-
-        prime.pb(2);
-        for(int i=3; i<mx; i+=2)
-          if(vis[i]==0)prime.pb(i);
-    }
-
-
-vector<int>prime_factor;
-void primeFactorization(int n)
+vector<int>prime;
+bool vis[mx];
+void sieve()
 {
-      for(int i=2; i*i<=n; i++)
+      for(ll i=3; i*i<=mx; i+=2)
       {
-          if(n%i==0)
+          if(vis[i]==0)
           {
-              while(n%i==0)
-              {
-                  n/=i;
-                  prime_factor.pb(i); 
-              }
+              for(ll j = i*i; j<mx; j+=2*i)
+              vis[j]=1;
           }
       }
-    if(n>1)prime_factor.pb(n);
+
+      prime.pb(2);
+      for(ll i=3; i<mx; i+=2)
+        if(vis[i]==0)prime.pb(i);
 }
+
+ll big_mod(ll base, ll power, ll mod)
+{
+    if(power==0)    return 1;
+    
+    else if(power%2==1) 
+    {
+        ll p1 = base % mod;
+        ll p2 = (big_mod(base,power-1,mod))%mod;
+        return (p1*p2)%mod;
+    }
+    else if(power%2==0)
+    {
+        ll p1 = (big_mod(base,power/2,mod))%mod;
+        return (p1*p1)%mod;
+    }
+}
+
 
 int main()
 {
@@ -77,28 +77,38 @@ int main()
         freopen("input.txt","r",stdin);
         freopen("ans.txt","w",stdout);
     #endif
-
-      int n;
+        sieve();
+        
+      ll n;
       while(cin>>n)
       {
          if(n==0)return 0;
-         if(n==1)cout<<1<<endl;
+        // if(n==1)cout<<1<<endl;
+        ll res = 1;
+        ll two=0, five=0;
 
-          primeFactorization(n);
-          for(int i=0; i<prime_factor.size(); i++)
-            cout<<prime_factor[i]<<" ";
-          // while(1)
-          // {
-          //   if(x%10!=0)
-          //   {
-          //     cout<<x%10<<endl;
-          //     break;
-          //   }
-          //   x/=10;
-          // }
+         for(ll i=0; prime[i]<=n; i++)
+         {
+           ll  pow = 0;
+            ll x = prime[i];
+            while(1)
+            {
+              if(x<=n) pow++;
+              else break;
+              x*=prime[i];
+            }
+            if(prime[i]==2) two = pow;
+            else if(prime[i]==5) five = pow;
+            else res*= big_mod(prime[i],pow, 10);
+            
+         }
+         res = res%10;
+         two = two-five;
+         two = big_mod(2,two,10);
+         res = (res*two)%10;
+         cout<<res<<endl;
+        // five = big_mod(5,five,10);
       }
-      
-         
        
     #ifdef EXTRA_8
         fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
