1861e7389790847fe217fae01ee5a3726e04b390
anikaksh <anik15-14210@diu.edu.bd>
2021-05-24 14:11:52 +0600

    is Prime Function

--- CodeWiki/Number Theory/isPrime.cpp
+++ CodeWiki/Number Theory/isPrime.cpp
@@ -1,18 +1,12 @@
 int is_prime(ll n)
 {
     ll i, root;
-    if(n==2)
-        return 1;
-    if(n%2==0 || n==1)
-        return 0;
+    if(n==2) return 1;
+    if(n%2==0 || n==1) return 0;
 
     root = sqrt(n);
-    for(i=3; i<=root; i = i+2)
-    {
-        if(n%i==0)
-        {
-            return 0;
-        }
-    }
+    
+    for(i=3; i<=root; i = i+2)if(n%i==0)  return 0;
+    
     return 1;
 }
