7be3c34a3b90d72f53a85b5368df9087e1667662
anikaksh <anik15-14210@diu.edu.bd>
2021-05-24 14:08:19 +0600

    Big Mod Function

---
+++ CodeWiki/Number Theory/Big MOD.cpp
@@ -0,0 +1,17 @@
+ll big_mod(ll base, ll power, ll mod)
+{
+    if(power==0)  return 1;
+    
+    else if(power%2==1) 
+    {
+        ll p1 = base % mod;
+        ll p2 = (big_mod(base,power-1,mod))%mod;
+        return (p1*p2)%mod;
+    }
+    else 
+    {
+        ll p1 = (big_mod(base,power/2,mod))%mod;
+        return (p1*p1)%mod;
+    }
+
+}
