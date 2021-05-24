567f5db3269debc12f701db25f4889d3bc15c6ed
anikaksh <anik15-14210@diu.edu.bd>
2021-05-24 18:12:16 +0600

    B no. Div 2 round 12

---
+++ CodeForces/correctSolution.cpp
@@ -0,0 +1,80 @@
+//             _   _                
+//   __ _ _   _| |_| |__   ___  _ __ 
+//  / _` | | | | __| '_ \ / _ \| '__|
+// | (_| | |_| | |_| | | | (_) | |   
+//  \__,_|\__,_|\__|_| |_|\___/|_|   
+//              _ _            _             _     
+//   __ _ _ __ (_) | __   __ _| | ____ _ ___| |__  
+//  / _` | '_ \| | |/ /  / _` | |/ / _` / __| '_ \ 
+// | (_| | | | | |   <  | (_| |   < (_| \__ \ | | |
+//  \__,_|_| |_|_|_|\_\  \__,_|_|\_\__,_|___/_| |_|
+
+#include<bits/stdc++.h>
+using namespace    std;
+
+#define flush                   cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define fasterio                ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                      cout<<endl;
+#define pi                      acos(-1.0) //3.1415926535897932384626
+#define dpoint(x)               fixed<<setprecision(x)
+#define debug(x)                cout<<x<<endl;
+// #define gcd(a, b)            __gcd(a, b)
+// #define lcm(a, b)            ((a)*((b)/gcd(a,b)))
+#define pb                      push_back
+#define mx                      1000006
+
+typedef long long int           ll;
+typedef double                  dl;
+typedef unsigned long long      ul;
+
+// --------------------------Pre made Functions & Proto Type--------------------------//
+template <class T> T digitsum(T n){T sum=0;while(n!=0){sum+=n%10;n/=10;}return sum;}
+int gcd(int a, int b){ int x ; return x = __gcd(a, b);}
+int lcm(int a, int b){int y; return y = ((a)*((b)/gcd(a,b)));}
+
+
+int main()
+{
+    #ifdef EXTRA_8
+        clock_t tStart = clock();
+        freopen("input.txt","r",stdin);
+        freopen("ans.txt","w",stdout);
+    #endif
+        string s1, s2;
+
+        cin>>s1>>s2;
+
+        sort(s1.begin(), s1.end());
+        if(s1[0]=='0')
+        {
+          for(int i=1; i<s1.size(); i++)
+          {
+              if(s1[i]!='0')
+              {
+                swap(s1[0],s1[i]);
+                break;
+              }
+          }
+        }
+        if(s1==s2)cout<<"OK"<<endl;
+        else cout<<"WRONG_ANSWER"<<endl;
+       
+    #ifdef EXTRA_8
+        fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+    #endif
+    return 0;
+      
+}
+
+
+//  _____            _   _            _                      ___   __ 
+// |  ___|__  _ __  | |_| |__   ___  | |    _____   _____   / _ \ / _|
+// | |_ / _ \| '__| | __| '_ \ / _ \ | |   / _ \ \ / / _ \ | | | | |_ 
+// |  _| (_) | |    | |_| | | |  __/ | |__| (_) \ V /  __/ | |_| |  _|
+// |_|  \___/|_|     \__|_| |_|\___| |_____\___/ \_/ \___|  \___/|_|                                                                    
+//   ____ ____  
+//  / ___|  _ \ 
+// | |   | |_) |
+// | |___|  __/ 
+//  \____|_|    
+//            
