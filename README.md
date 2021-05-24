99a0cd7c2302a2958b07b577eb09d251d927c3c9
anikaksh <anik15-14210@diu.edu.bd>
2021-05-24 17:05:54 +0600

    Update Readme

--- README.md
+++ README.md
@@ -7,8 +7,24 @@
 </head>
 <body>
 	<h1>My All Competitive Programming Solve</h1> 
-	
-	
+	<p>Easy Access From Here</p>
+	<div>
+		<ol Type = "1">
+		  <li href="Sports-Programming\AtCoder">At Code</li>
+		  <li href="Sports-Programming\CodeChef">Code Chef</li>
+		  <li>Code Forces</li>
+		  <li>Dimik Oj</li>
+		  <li>Hacker Rank</li>
+		  <li>ICPC LA</li>
+		  <li>Light Oj</li>
+		  <li>SPOJ</li>
+		  <li>Times</li>
+		  <li>TopH</li>
+		  <li>URI Online Judge</li>
+		  <li>UVA Online Judge</li>
+		</ol>
+	</div>
+
 	
 </body>
 </html>
--- Template/Main.cpp
+++ Template/Main.cpp
@@ -9,66 +9,28 @@
 // | (_| | | | | |   <  | (_| |   < (_| \__ \ | | |
 //  \__,_|_| |_|_|_|\_\  \__,_|_|\_\__,_|___/_| |_|
 
-
-
 #include<bits/stdc++.h>
 using namespace    std;
 
-#define flush             cin.ignore(numeric_limits<streamsize>::max(),'\n')
-#define fasterio          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define NL                cout<<endl;
-#define pi                acos(-1.0) //3.1415926535897932384626
-#define dpoint(x)         fixed<<setprecision(x)
-#define debug(x)          cout<<x<<endl;
-// #define gcd(a, b)         __gcd(a, b)
-// #define lcm(a, b)        ((a)*((b)/gcd(a,b)))
-#define pb                push_back
-#define mx                1000006
+#define flush                   cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define fasterio                ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                      cout<<endl;
+#define pi                      acos(-1.0) //3.1415926535897932384626
+#define dpoint(x)               fixed<<setprecision(x)
+#define debug(x)                cout<<x<<endl;
+// #define gcd(a, b)            __gcd(a, b)
+// #define lcm(a, b)            ((a)*((b)/gcd(a,b)))
+#define pb                      push_back
+#define mx                      1000006
 
-typedef long long int       ll;
-typedef double              dl;
-typedef unsigned long long  ul;
+typedef long long int           ll;
+typedef double                  dl;
+typedef unsigned long long      ul;
 
 // --------------------------Pre made Functions & Proto Type--------------------------//
 template <class T> T digitsum(T n){T sum=0;while(n!=0){sum+=n%10;n/=10;}return sum;}
 int gcd(int a, int b){ int x ; return x = __gcd(a, b);}
 int lcm(int a, int b){int y; return y = ((a)*((b)/gcd(a,b)));}
-
-vector<int>prime;
-bool vis[mx];
-void sieve()
-{
-      for(ll i=3; i*i<=mx; i+=2)
-      {
-          if(vis[i]==0)
-          {
-              for(ll j = i*i; j<mx; j+=2*i)
-              vis[j]=1;
-          }
-      }
-
-      prime.pb(2);
-      for(ll i=3; i<mx; i+=2)
-        if(vis[i]==0)prime.pb(i);
-}
-
-ll big_mod(ll base, ll power, ll mod)
-{
-    if(power==0)  return 1;
-    
-    else if(power%2==1) 
-    {
-        ll p1 = base % mod;
-        ll p2 = (big_mod(base,power-1,mod))%mod;
-        return (p1*p2)%mod;
-    }
-    else 
-    {
-        ll p1 = (big_mod(base,power/2,mod))%mod;
-        return (p1*p1)%mod;
-    }
-
-}
 
 
 int main()
@@ -78,35 +40,7 @@ int main()
         freopen("input.txt","r",stdin);
         freopen("ans.txt","w",stdout);
     #endif
-        sieve();
         
-      ll n;
-      while(cin>>n)
-      {
-         if(n==0)return 0;
-        ll res = 1;
-        ll two=0, five=0;
-
-         for(ll i=0; prime[i]<=n; i++)
-         {
-           ll  pow = 0;
-            ll x = prime[i];
-            while(1)
-            {
-              if(x<=n) pow++;
-              else break;
-              x*=prime[i];
-            }
-            if(prime[i]==2) two = pow;
-            else if(prime[i]==5) five = pow;
-            else res = ((res%10)*(big_mod(prime[i],pow, 10)))%10;
-         }
-
-         two = two-five;
-         two = big_mod(2,two,10);
-         res = (res*two)%10;
-         cout<<res<<endl;
-      }
        
     #ifdef EXTRA_8
         fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
