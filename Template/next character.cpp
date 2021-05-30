7431b29991dc123a60e77d2f1976def3c4457c99
anikakash <anik15-14210@diu.edu.bd>
2021-05-30 18:56:28 +0600

    control statemetn

--- Template/next character.cpp
+++ Template/next character.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include<string.h>
-
+#include <ctype.h>
 int main()
 {
 	#ifdef anikakash
@@ -8,10 +8,24 @@ int main()
         freopen("input.txt","r",stdin);
         freopen("out.txt","w",stdout);
    #endif
-	printf("Enter a string : \n");
-		char arr[50];
-		scanf("%s", arr);
-		for(int i=0; i<strlen(arr); i++)
-			printf("%c",arr[i]+1);
+
+	printf("Enter Test Case : \n");
+		int n; 
+		scanf("%d",&n);
+		for(int i=1; i<=n; i++)
+		{
+			char ch;
+			scanf("\n%c",&ch);
+			if(isdigit(ch))printf("%d is a Digit\n", ch);
+			else if((ch >='A' &&  ch<='Z') || (ch >='a' &&  ch<='z'))
+			{
+				printf("%C is an Alphabet : ", ch);
+				if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+					printf("It is an Vowel\n");
+				else printf("It is an Consonant\n");
+			}
+			else printf("%c is a special character\n",ch);
+		}
+		
 	return 0;
 }
