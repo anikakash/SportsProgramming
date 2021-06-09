3d892aa11758968bb2353767dc64a4974eceaad9
anikakash <anik15-14210@diu.edu.bd>
2021-06-09 13:34:51 +0600

    remove only lower bound and make both one

--- CodeWiki/BinarySearch/Upper and Lower Bound Binary search.cpp
+++ CodeWiki/BinarySearch/Upper and Lower Bound Binary search.cpp
@@ -23,6 +23,21 @@ int upperBound(int key)
    return ans;
 }
 
+int lowerBound(int key)
+{
+   int l=0, r = arr.size()-1, ans=-1;
+   while(l<=r)
+   {
+      int mid = l+(r-l)/2;
+      if(arr[mid]>=key)
+      {
+         ans = mid;
+         r = mid-1;
+      }
+      else l = mid+1;
+   }
+   return ans;
+}
 
 int main()
 {
