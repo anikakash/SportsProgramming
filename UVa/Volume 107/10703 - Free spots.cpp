4f417e49758738b26bbc37ae1b28ece3537b7c86
anikakash <anik15-14210@diu.edu.bd>
2021-06-28 01:12:04 +0600

    simulations

---
+++ UVa/Volume 107/10703 - Free spots.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace    std;
+
+#define flush                   cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define FASTERIO                ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                      cout<<endl;
+#define pi                      acos(-1.0) //3.1415926535897932384626
+#define pb                      push_back
+#define mx                      10000007
+#define EPS                     1e-10
+#define dpoint(x)               fixed<<setprecision(x)
+typedef long long int           ll;
+typedef double                  dl;
+typedef unsigned long long      ul;
+template <class T> T digitsum(T n){T sum=0;while(n!=0){sum+=n%10;n/=10;}return sum;}
+int gcd(int a, int b){ int x ; return x = __gcd(a, b);}
+int lcm(int a, int b){int y; return y = ((a)*((b)/gcd(a,b)));}
+
+// Debugger 
+#define debugNS(a,b)             cout<<a<<" = "<<b<<endl;
+#define debugN(b)               cout<<b<<endl;
+
+
+
+int main(){
+   #ifdef anikakash
+        clock_t tStart = clock();
+        freopen("in.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+   #endif
+     
+     int w,h, n;
+     while(cin>>w>>h>>n){
+
+      if(w==0 && h==0 && n==0) return 0;
+
+         int arr[505][505]={0};
+         while(n--){
+            int x1, y1, x2, y2;
+            cin>>x1>>y1>>x2>>y2;
+
+            if(x2>x1)swap(x1,x2);
+            if(y2>y1)swap(y1,y2);
+
+            for(int i=y2; i<=y1; i++){
+               for(int j=x2; j<=x1; j++){
+                  arr[i][j]=true;
+               }
+            }
+         }
+            int cnt=0;
+            for(int i=1; i<=h; i++){
+               for(int j=1; j<=w; j++){
+                  if(arr[i][j]==false)cnt++;
+               }
+            }
+         if(cnt==0)cout<<"There is no empty spots."<<endl;
+         else if(cnt==1)cout<<"There is one empty spot."<<endl;
+         else cout<<"There are "<<cnt<<" empty spots."<<endl;
+     }
+
+    
+
+        
+   #ifdef anikakash
+      fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+   #endif
+   return 0;
+}
