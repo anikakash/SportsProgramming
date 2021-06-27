b9bfced086edf37af91f6c41bb955593e4e6732b
anikakash <anik15-14210@diu.edu.bd>
2021-06-28 01:11:40 +0600

    Simulations

---
+++ CodeForces/A. Patrick and Shopping.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace    std;
+
+#define flush                   cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define FASTERIO                ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                      cout<<endl;
+#define pi                      acos(-1.0) //3.1415926535897932384626
+#define pb                      push_back
+#define mx                      10000007
+#define EPS                     1e-10
+#define dpoint(x)               fixed<<setprecision(x)
+#define debug1(a)               cout<<" "<<a<<" = ";
+#define debug2(b)               cout<<b<<endl;
+typedef long long int           ll;
+typedef double                  dl;
+typedef unsigned long long      ul;
+template <class T> T digitsum(T n){T sum=0;while(n!=0){sum+=n%10;n/=10;}return sum;}
+int gcd(int a, int b){ int x ; return x = __gcd(a, b);}
+int lcm(int a, int b){int y; return y = ((a)*((b)/gcd(a,b)));}
+
+
+int main(){
+   #ifdef anikakash
+        clock_t tStart = clock();
+        freopen("in.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+   #endif
+     
+     ll d1, d2, d3;
+     cin>>d1>>d2>>d3;
+
+     ll c1 = d1+d2+d3;
+     ll c2 = (d1+d3)*2;
+     ll c3 = (d1+d2)*2;
+     ll c4 = (d2+d3)*2;
+     //cout<<c1<<" "<<c2<<" "<<c3<<" "<<c4<<endl;
+      vector<ll>arr;
+      arr.pb(c1);
+      arr.pb(c2);
+      arr.pb(c3);
+      arr.pb(c4);
+
+      sort(arr.begin(), arr.end());
+      cout<<arr[0]<<endl;
+        
+   #ifdef anikakash
+      fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+   #endif
+   return 0;
+}
