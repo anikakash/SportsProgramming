7887e8bb5ee996af90d389a3c15392aca17c673a
anikakash <anik15-14210@diu.edu.bd>
2021-08-05 12:49:19 +0600

    lowest swap

---
+++ LightOj/1166-Old Sorting.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace    std;
+
+#define flush                   cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define FASTERIO                ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                      cout<<'\n';
+#define pi                      acos(-1.0) //3.1415926535897932384626
+#define pb                      push_back
+#define mx                      10000007
+#define AC                      int
+#define EPS                     1e-10
+#define dpoint(x)               fixed<<setprecision(x)
+typedef long long int           ll;
+typedef double                  dl;
+typedef unsigned long long      ul;
+template <class T> T digitsum(T n){T sum=0;while(n!=0){sum+=n%10;n/=10;}return sum;}
+int gcd(int a, int b){ int x ; return x = __gcd(a, b);}
+int lcm(int a, int b){int y; return y = ((a)*((b)/gcd(a,b)));}
+
+// Debugger
+#define debugNS(a,b)             cout<<a<<" = "<<b<<endl;
+#define debugN(b)               cout<<b<<endl;
+
+
+int main(){
+   
+  #ifdef anikakash
+       clock_t tStart = clock();
+       freopen("INPUT.txt","r",stdin);
+       freopen("OUTPUT.txt","w",stdout);
+  #endif
+
+    FASTERIO;
+        
+        int tt; cin>>tt;
+        for(int caseno=1; caseno<=tt; caseno++){
+            int n;cin>>n;
+            vector<int>v(n);
+            for(int i=0; i<n; i++)cin>>v[i];
+                int cnt=0;
+            for(int i=0; i<n; i++){
+                int tmp1 = v[i];
+                for(int j=i+1; j<n; j++){
+                    if(v[j]==i+1){
+                        int tmp2 = v[j]; 
+                        v[j] = tmp1;
+                        v[i] = tmp2;
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            cout<<"Case "<<caseno<<": "<<cnt<<endl;
+        }
+
+
+  #ifdef anikakash
+     fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+  #endif
+   return 0;
+}
+/*
+   
+
+
+*/
