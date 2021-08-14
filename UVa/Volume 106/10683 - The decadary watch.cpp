a1ff823469063d075ae73ebe15a9c017c31c1039
anikakash <anik15-14210@diu.edu.bd>
2021-08-14 19:27:15 +0600

    uva easy

--- UVa/Volume 106/10683 - The decadary watch.cpp
+++ UVa/Volume 106/10683 - The decadary watch.cpp
@@ -23,38 +23,6 @@ int lcm(int a, int b){int y; return y = ((a)*((b)/gcd(a,b)));}
 #define debugNS(a,b,c)             cout<<a<<b<<c<<endl;
 #define debugN(b)               cout<<b<<endl;
 
-int dx[] = {0, 0, -1, 1, -1, -1, 1, 1}; //Graph Move;
-int dy[] = {1, -1, 0, 0, -1, 1, -1, 1};
-
-string DecimalToBinary(int n){
-
-    string res="";
-    for(int i=31; i>=0; i--){
-        int k = n & (1<<i);
-        res += (n&k)?'1':'0';
-    }
-    return res;
-}
-int BinaryToDecimal(string s){
-    int ans = 0;
-    for(int i=0; i<s.size(); i++){
-        ans = (2*ans)+s[i]-'0';
-    }
-    return ans;
-}
-
- bool cmp(pair<int,char>a, pair<int,char>b){
-    if(a.first!=b.first) return a>b;
-    return a<b;
- }
-
-ll findTrailingZeros(ll n)
-{
-    ll count = 0;
-    for (ll i = 5; n / i >= 1; i *= 5)
-        count += n / i;
-    return count;
-}
 
 int main(){
    
@@ -74,6 +42,7 @@ int main(){
         sec+= ((s[6]-'0')*10 + (s[7]-'0'));
 
         printf("%07d\n",((sec*(10*100*100*100))/(24*60*60*100)));
+        // Normal one in cc is-> (24 * 60 * 60 * 100); But in set Day is (10*100*100*100)
     }
 
   #ifdef anikakash
