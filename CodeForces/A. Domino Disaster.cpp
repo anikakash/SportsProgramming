1bc0f4e1016a71857eaa4b95052272fdd1fee2ad
anikakash <anik15-14210@diu.edu.bd>
2021-09-06 13:26:02 +0600

    div 2 r 742 a

---
+++ CodeForces/A. Domino Disaster.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace    std;
+
+#define flush                    cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define FASTERIO                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                       cout<<'\n';
+#define pi                       acos(-1.0) //3.1415926535897932384626
+#define pb                       push_back
+#define mk                       make_pair
+#define mx                       1e2
+#define EPS                      1e-10
+#define dpoint(x)                fixed<<setprecision(x)
+typedef long long int            ll;
+typedef double                   dl;
+typedef unsigned long long int   ull;
+
+//Funtions
+template <class T> T digitsum(T n){T sum=0;while(n!=0){sum+=n%10;n/=10;}return sum;}
+int gcd(int a, int b){ int x ; return x = __gcd(a, b);}
+int lcm(int a, int b){int y; return y = ((a)*((b)/gcd(a,b)));}
+
+// Debugger
+#define gobug                   0
+#define debugNS(a,b,c)          cout<<a<<b<<c<<endl;
+#define debugN(b)               cout<<b<<endl;
+
+
+
+int main(){
+   
+  #ifdef anikakash
+       clock_t tStart = clock();
+       freopen("INPUT.txt","r",stdin);
+       freopen("output.txt","w",stdout); 
+  #endif
+
+  FASTERIO; //cmt when use scanf & printf ;
+  
+   int tt;cin>>tt;
+   while(tt--){
+        int n;cin>>n;
+        flush;
+        string s, s2=""; cin>>s;
+
+        for(int i=0; i<n; i++){
+            if(s[i]=='U')s2.pb('D');
+            else if(s[i]=='D')s2.pb('U');
+            else s2.pb(s[i]);
+        }
+        cout<<s2<<endl;
+      
+   }
+
+  #ifdef anikakash
+     fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+  #endif
+
+   return 0;
+}
