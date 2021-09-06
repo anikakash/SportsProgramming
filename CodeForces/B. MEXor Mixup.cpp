140fb75c8b99c10a0ec48c7a506099a455c36a71
anikakash <anik15-14210@diu.edu.bd>
2021-09-06 13:25:04 +0600

    div 2 r 742 B

---
+++ CodeForces/B. MEXor Mixup.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace    std;
+
+#define flush                    cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define FASTERIO                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                       cout<<'\n';
+#define pi                       acos(-1.0) //3.1415926535897932384626
+#define pb                       push_back
+#define mk                       make_pair
+#define mx                       1e2
+#define EPS                      1e-10
+#define dpoint(x)                fixed<<setprecision(x)
+typedef long long int            ll;
+typedef double                   dl;
+typedef unsigned long long int   ull;
+
+//Funtions
+template <class T> T digitsum(T n){T sum=0;while(n!=0){sum+=n%10;n/=10;}return sum;}
+int gcd(int a, int b){ int x ; return x = __gcd(a, b);}
+int lcm(int a, int b){int y; return y = ((a)*((b)/gcd(a,b)));}
+
+// Debugger
+#define gobug                   0
+#define debugNS(a,b,c)          cout<<a<<b<<c<<endl;
+#define debugN(b)               cout<<b<<endl;
+
+int xoo(int n){
+    if(n%4==0)return n;
+    else if(n%4==1)return 1;
+    else if(n%4==2)return n+1;
+    return 0;
+}
+
+int main(){
+   
+  #ifdef anikakash
+       clock_t tStart = clock();
+       freopen("INPUT.txt","r",stdin);
+       freopen("output.txt","w",stdout); 
+  #endif
+
+  FASTERIO; //cmt when use scanf & printf ;
+  
+   int tt;cin>>tt;
+   while(tt--){
+        int a,b; cin>>a>>b;
+        int x = xoo(a-1);
+        if(x==b)cout<<a<<endl;
+        else if((x^b)!= a)cout<<a+1<<endl;
+        else cout<<a+2<<endl;
+   }
+
+  #ifdef anikakash
+     fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+  #endif
+
+   return 0;
+}
