c4abf60f62f43557d9c07fd037066fc14fc15396
anikakash <anik15-14210@diu.edu.bd>
2021-10-15 22:58:05 +0600

    abc 217 -> a,b

---
+++ AtCoder/AtCoder Beginner Contest 217/B - AtCoder Quiz.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace    std;
+#define flush                    cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define FASTERIO                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                       cout<<'\n';
+#define pi                       acos(-1.0) //3.1415926535897932384626
+#define pb                       push_back
+#define mk                       make_pair
+#define mx                       20000000
+#define EPS                      1e-10
+#define dpoint(x)                fixed<<setprecision(x)
+typedef long long int            ll;
+typedef double                   dl;
+typedef unsigned long long int   ull;
+const ll  MOD = 1e9 + 7;
+//Funtions
+template <class T> T digitsum(T n) {T sum = 0; while (n != 0) {sum += n % 10; n /= 10;} return sum;}
+int gcd(int a, int b) { int x ; return x = __gcd(a, b);}
+int lcm(int a, int b) {int y; return y = ((a) * ((b) / gcd(a, b)));}
+
+// Debugger
+#define gobug                   0
+#define debugNS(a,b,c)          cout<<a<<b<<c<<endl;
+#define debugN(b)               cout<<b<<endl;
+
+
+int main() {
+
+#ifdef anikakash
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    FASTERIO; //cmt when use scanf & printf ;
+
+   // string s = "ABC""ARC""AGC""AHC";
+   char list[4][4]= {"ABC","ARC","AGC","AHC"};
+   // sort(list, list+4);
+
+   string s1, s2, s3; cin>>s1>>s2>>s3;
+
+   bool vis[4]={false};
+   // for(int i=0; i<4; i++)cout<<vis[i]<<" ";
+   for(int i=0; i<4; i++)if(s1==list[i])vis[i] = true;
+   for(int i=0; i<4; i++)if(s2==list[i])vis[i] = true;
+   for(int i=0; i<4; i++)if(s3==list[i])vis[i] = true;
+
+    for(int i=0; i<4; i++)if(!vis[i])cout<<list[i]<<endl;
+   
+
+
+#ifdef anikakash
+    fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+
+    return 0;
+}
