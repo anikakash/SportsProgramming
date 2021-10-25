0bb64b174038bc4175b706bc0e5fd5f1e0075aaf
anikakash <anik15-14210@diu.edu.bd>
2021-10-26 00:56:55 +0600

    cf div 2 751 A no

---
+++ CodeForces/A. Two Subsequences.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace    std;
+
+#define flush                    cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define FASTERIO                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                       cout<<'\n';
+// #define pi                       acos(-1.0) //3.1415926535897932384626
+#define pb                       push_back
+#define mk                       make_pair
+#define mx                       46500
+#define EPS                      1e-18
+#define dpoint(x)                fixed<<setprecision(x)
+#define Fill(ar, val)            memset(ar, val, sizeof(ar))
+typedef long long int            ll;
+typedef double                   dl;
+typedef unsigned long long int   ull;
+
+//Funtions
+template <class T> T digitsum(T n) {T sum = 0; while (n != 0) {sum += n % 10; n /= 10;} return sum;}
+template <class T> T gcd(T a, T b) { T x ; return x = __gcd(a, b);}
+template <class T> T lcm(T a, T b) {T y; return y = ((a) * (b)) / gcd(a, b);}
+
+// Debugger
+#define gobug                   0
+#define debugNS(a,b,c)          cout<<a<<b<<c<<endl;
+#define debugN(b)               cout<<b<<endl;
+
+int ROW[] = { +0, +0, -1, +1};
+int COL[] = { +1, -1, +0, +0};
+
+int X[] = { +0, +0, +1, -1, -1, +1, -1, +1}; // Kings Move
+int Y[] = { -1, +1, +0, +0, +1, +1, -1, -1}; // Kings Move
+
+int KX[] = { -2, -2, -1, -1,  1,  1,  2,  2}; // Knights Move
+int KY[] = { -1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+
+
+bool cmp(pair<int, int>a, pair<int, int>b) {
+    if (a < b)return true;
+    return false;
+}
+
+
+int main() {
+
+#ifdef anikakash
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("ans.txt", "w", stdout);
+#endif
+
+    FASTERIO;
+
+    int tt; cin >> tt;
+    while (tt--) {
+        string s; cin >> s;
+        string s1 = s;
+        sort(s1.begin(), s1.end());
+        cout<<s1[0]<<" ";
+        bool flg = false;
+
+        for(int i=0; i<s.size(); i++){
+            if(s1[0]!=s[i] || flg== true)cout<<s[i];
+            if(s1[0]==s[i] && flg == false){
+                flg = true;
+            }
+        }
+        cout<<endl;
+    }
+
+#ifdef anikakash
+    fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+
+    return 0;
+}
