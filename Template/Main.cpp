7a5263f8b82d0de7f52a46f194c5aa82896b6997
anikakash <anik15-14210@diu.edu.bd>
2021-12-03 02:34:36 +0600

    practice

--- Template/Main.cpp
+++ Template/Main.cpp
@@ -47,31 +47,48 @@ int main() {
 #ifdef anikakash
     clock_t tStart = clock();
     freopen("input.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
+    freopen("ans.txt", "w", stdout);
 #endif
 
     FASTERIO;
-    int tt; cin>>tt;
-    for(int caseno=1; caseno<=tt; caseno++){
-        int n; cin>>n;
-        int arr[1005][1005];
-        int dp[1005][1005];
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=3; j++){
-                cin>>arr[i][j];
+    int tt; cin >> tt;
+    for (int caseno = 1; caseno <= tt; caseno++) {
+        ll n, cnt = 2; cin >> n;
+        ll arr[2 * n + 2][2 * n + 2] = {0};
+        ll dp[2 * n + 2][2 * n + 2] = {0};
+
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int xx = i;
+            if (xx > n) {
+                xx -= cnt;
+                cnt += 2;
+            }
+            for (int j = 1; j <= xx; j++) {
+                cin >> arr[i][j];
+            }
+        }
+        dp[1][1] = arr[1][1];
+        for (int i = 2; i <= 2 * n - 1; i++) {
+            for (int j = 1; j <= 2 * n - 1; j++) {
+                if (i <= n) {
+                    if (j == 1)dp[i][j] = arr[i][j] + dp[i - 1][j];
+                    else {
+                        dp[i][j] = max(dp[i - 1][j - 1] + arr[i][j], dp[i - 1][j] + arr[i][j]);
+                    }
+                }
+                else{
+                    dp[i][j] = max(dp[i - 1][j] + arr[i][j], dp[i - 1][j+1] + arr[i][j]);
+                }
             }
         }
 
-       dp[1][1]=arr[1][1];
-       dp[1][2]=arr[1][2];
-       dp[1][3]=arr[1][3];
-
-       for(int i=2; i<=n; i++){
-            dp[i][1] = min(dp[i-1][2], dp[i-1][3])+arr[i][1];
-            dp[i][2] = min(dp[i-1][1], dp[i-1][3])+arr[i][2];
-            dp[i][3] = min(dp[i-1][1], dp[i-1][2])+arr[i][3];
-       }
-       cout<<"Case "<<caseno<<": "<<(int)min(dp[n][1],min(dp[n][2],dp[n][3]))<<endl;
+        // for (int i = 1; i <= 2 * n - 1; i++) {
+        //     for (int j = 1; j <= 2 * n - 1; j++) {
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        cout << "Case " << caseno << ": " << dp[2 * n - 1][1] << endl;
     }
 
 
