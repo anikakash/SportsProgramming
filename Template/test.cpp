9c0b23ac84d12a155a68467856b98a0bb8db7cc8
anikakash <anik15-14210@diu.edu.bd>
2021-12-27 12:50:54 +0600

    div 3 round 762 b

--- Template/test.cpp
+++ Template/test.cpp
@@ -82,11 +82,19 @@ int main() {
     freopen("ans.txt", "w", stdout);
 #endif
 
-    FASTERIO;
-    PreCal();
-    int n;
-    while(cin>>n){
-        cout<<ans[n]<<endl;
+    int tt; cin>>tt;
+    while(tt--){
+        int x1, x2, y1, y2;
+        cin>>x1>>y1>>x2>>y2;
+        if((x1%2==0 && y1%2==0) && (x2%2==0 && y2%2==0))cout<<"YES"<<endl;
+        else if((x1%2==0 && y1%2==0) && (x2%2==1 && y2%2==1))cout<<"YES"<<endl;
+        else if((x1%2==0 && y1%2==1) && (x2%2==0 && y2%2==1))cout<<"YES"<<endl;
+        else if((x1%2==0 && y1%2==1) && (x2%2==1 && y2%2==0))cout<<"YES"<<endl;
+        else if((x1%2==1 && y1%2==1) && (x2%2==1 && y2%2==1))cout<<"YES"<<endl;
+        else if((x1%2==1 && y1%2==1) && (x2%2==0 && y2%2==0))cout<<"YES"<<endl;
+        else if((x1%2==1 && y1%2==0) && (x2%2==1 && y2%2==0))cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+
     }
 
 #ifdef anikakash
