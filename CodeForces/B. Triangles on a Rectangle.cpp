2127898c089c3916b55d1c441abe4c96f0c197a6
anikakash <anik15-14210@diu.edu.bd>
2022-01-02 22:03:52 +0600

    DIV 2 EDU R-119 B

---
+++ CodeForces/B. Triangles on a Rectangle.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace    std;
+
+#define flush                    cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define FASTERIO                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                       cout<<'\n';
+// #define pi                       acos(-1.0) //3.1415926535897932384626
+#define pb                       push_back
+#define mk                       make_pair
+#define MaxN                     1000000
+#define EPS                      1e-18
+#define dpoint(x)                fixed<<setprecision(x)
+#define Fill(ar, weight)         memset(ar, weight, sizeof(ar))
+typedef long long int            ll;
+typedef double                   dl;
+typedef unsigned long long int   ull;
+
+//Funtions
+template <class T> T digitsum(T n) {T sum = 0; while (n != 0) {sum += n % 10; n /= 10;} return sum;}
+template <class T> T gcd(T a, T b) { T x ; return x = __gcd(a, b);}
+template <class T> T lcm(T a, T b) {T y; return y = ((a) * (b)) / gcd(a, b);}
+
+// Debugger
+#define gobug                   0
+#define debugNS(a,b,c)          cout<<a<<b<<c<<endl;
+#define debugN(b)               cout<<b<<endl;
+
+int ROW[] = { +0, +0, -1, +1};
+int COL[] = { +1, -1, +0, +0};
+
+
+
+int main() {
+
+#ifdef anikakash
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("ans.txt", "w", stdout);
+#endif
+
+    FASTERIO;
+
+    int tt;cin>>tt;
+    while(tt--){
+        ll w, h; cin>>w>>h;
+
+        ll ans1=0;
+        for(int i=0; i<2; i++){
+            ll n; cin>>n;
+            vector<ll>v(n);
+            for(int i=0; i<n; i++)cin>>v[i];
+            sort(v.begin(), v.end());
+            ans1 = max(ans1, v[n-1]-v[0]);
+        }
+        ll ans2=0;
+        for(int i=0; i<2; i++){
+            ll n; cin>>n;
+            vector<ll>v(n);
+            for(int i=0; i<n; i++)cin>>v[i];
+            sort(v.begin(), v.end());
+            ans2 = max(ans2, v[n-1]-v[0]);
+        }
+        cout<<(ll)max(ans2*w, ans1*h)<<endl;
+    }
+
+#ifdef anikakash
+    fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+    return 0;
+}
