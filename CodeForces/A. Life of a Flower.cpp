4864f92c9b3e467c12e5e7107835add06fadf3b7
anikakash <anik15-14210@diu.edu.bd>
2022-01-08 22:51:49 +0600

    DIV 2 Round 759 A

---
+++ CodeForces/A. Life of a Flower.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace    std;
+
+#define flush                    cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define FASTERIO                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                       cout<<'\n';
+// #define pi                       acos(-1.0) //3.1415926535897932384626
+#define pb                       push_back
+#define mk                       make_pair
+#define MaxN                     1000000
+#define EPS                      1e-18
+#define dpoint(x)                fixed<<setprecision(x)
+#define Fill(ar, weight)         memset(ar, weight, sizeof(ar))
+typedef long long int            ll;
+typedef double                   dl;
+typedef unsigned long long int   ull;
+
+//Funtions
+template <class T> T digitsum(T n) {T sum = 0; while (n != 0) {sum += n % 10; n /= 10;} return sum;}
+template <class T> T gcd(T a, T b) { T x ; return x = __gcd(a, b);}
+template <class T> T lcm(T a, T b) {T y; return y = ((a) * (b)) / gcd(a, b);}
+
+// Debugger
+#define gobug                   0
+#define debugNS(a,b,c)          cout<<a<<b<<c<<endl;
+#define debugN(b)               cout<<b<<endl;
+
+void solve() {
+    int n; cin>>n;
+    vector<int>v(n);
+    for(int i=0; i<n; i++)cin>>v[i];
+        int ans = 1;
+    // for(auto it:v)cout<<it<<" ";
+    //     cout<<endl;
+
+    for(int i=0; i<n; i++){
+        if(v[i]==1 && v[i+1]==1 && i<n-1){
+            ans+=5;
+        }
+        else if(v[i]==1)ans++;
+        else if(v[i]==0 && v[i+1]==0 && i<n-1){
+            ans=-1; break;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+
+#ifdef anikakash
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("ans.txt", "w", stdout);
+#endif
+
+    FASTERIO;
+
+    int tt; cin >> tt;
+    while (tt--) {
+        solve();
+    }
+#ifdef anikakash
+    fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+    return 0;
+}
