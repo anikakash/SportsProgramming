dbe34cf962f8031b91f47d9cb4e83375ab501fb3
anikakash <anik15-14210@diu.edu.bd>
2022-07-23 14:10:36 +0600

    update

--- Template/nobita.cpp
+++ Template/nobita.cpp
@@ -18,32 +18,40 @@ typedef unsigned long long int   ull;
 int main() {
 
     FASTERIO;
-    int t; cin>>t;
-    while(t--){
- int n, m; cin>>n>>m;
-   vector<int>v(n);
-   for(int i=0; i<n; i++)cin>>v[i];
-    string ss = "";
-    for(int i=0; i<m; i++)ss.pb('B');
-    for(int i=0; i<n; i++){
-        int x = v[i]-1;
-        int y = (m+1)-v[i];
-            y--;
-            // cout<<x<<" "<<y<<endl;
-       if(x<m && ss[x]=='B'){
-        if(y<x){
-            if(ss[y]=='B')ss[y]='A';
-            else ss[x]='A';
-        }
-        else ss[x]='A';
-        // ss[x]='A';
-       }
-       else if(y<m && ss[y]=='B')ss[y]='A';
-    }
-    // reverse(ss.begin(), ss.end());
-    cout<<ss<<endl;
-        
+    int n, m; cin>>n>>m;
+    vector<int>v(n);
+    for(int i=0; i<n; i++)cin>>v[i];
+    int sum1[n+5]={0};
 
+    for(int i=2; i<=n; i++){
+        if(v[i-2]>v[i-1]){
+            sum1[i]=sum1[i-1]+(v[i-2]-v[i-1]);
+        }
+        else sum1[i]=sum1[i-1];
+    }
+    // for(int i=1; i<=n; i++)cout<<sum1[i]<<" ";NL;
+    reverse(v.begin(), v.end());
+    
+    vector<int>sum2;
+    sum2.pb(0);
+    sum2.pb(0);
+    for(int i=2; i<=n; i++){
+        if(v[i-2]>v[i-1]){
+            sum2.pb(sum2[i-1]+(v[i-2]-v[i-1]));
+        }
+        else sum2.pb(sum2[i-1]);
+    }
+    reverse(sum2.begin(), sum2.end());
+    // for(int i=0; i<sum2.size(); i++)cout<<sum2[i]<<" ";NL;
+    while(m--){
+        int x, y; cin>>x>>y;
+        if(x<y){
+            // cout<<sum1[x]<<" "<<sum1[y]<<endl;
+            cout<<abs((sum1[y]-sum1[x])-sum1[x-1])<<endl;
+        }
+        else{
+            cout<<abs((sum2[y-1]-sum2[x-1])-sum1[x-2])<<endl;
+        }
     }
     return 0;
     
