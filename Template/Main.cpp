76bb68ffcceeee4a08a1ee3df0083e6058ecab2f
anikakash <anik15-14210@diu.edu.bd>
2022-12-25 13:01:57 +0600

    updating template for new beginning

--- Template/Main.cpp
+++ Template/Main.cpp
@@ -15,76 +15,22 @@ typedef long long int            ll;
 typedef double                   dl;
 typedef unsigned long long int   ull;
 
-void printSubstrings(string str)
-{
- 
-    // First loop for starting index
-    string ans ="";
-    for (int i = 0; i < str.length(); i++) {
-        string subStr="";
-        // Second loop is generating sub-string
-        for (int j = i; j < str.length(); j++) {
-            subStr += str[j];
-           string ss = subStr;
-           reverse(ss.begin(), ss.end());
-           if(ss==subStr){
-                if(subStr.size()>ans.size())ans = subStr;
-           }
-        }
-    }
-    cout<<ans<<endl;
-}
-
 void solve(){
-    ll n, m; cin>>n>>m;
-    vector<ll>v(n);
-    for(int i=0; i<n; i++)cin>>v[i];
-    ll sum1[n+5]={0};
-
-    for(int i=2; i<=n; i++){
-        if(v[i-2]>v[i-1]){
-            sum1[i]=sum1[i-1]+(v[i-2]-v[i-1]);
-        }
-        else sum1[i]=sum1[i-1];
-    }
-    // for(int i=1; i<=n; i++)cout<<sum1[i]<<" ";NL;
-    reverse(v.begin(), v.end());
-    
-    vector<ll>sum2;
-    sum2.pb(0);
-    sum2.pb(0);
-    for(int i=2; i<=n; i++){
-        if(v[i-2]>v[i-1]){
-            sum2.pb(sum2[i-1]+(v[i-2]-v[i-1]));
-        }
-        else sum2.pb(sum2[i-1]);
-    }
-    reverse(sum2.begin(), sum2.end());
-    // for(int i=0; i<sum2.size(); i++)cout<<sum2[i]<<" ";NL;
-    while(m--){
-        ll x, y; cin>>x>>y;
-        if(x<y){
-            // cout<<sum1[x]<<" "<<sum1[y]<<endl;
-            cout<<(ll)abs(sum1[y]-sum1[x])<<endl;
-        }
-        else{
-            cout<<(ll)abs(sum2[y-1]-sum2[x-1])<<endl;
-        }
-    }
+    int n; cin>>n;
+    cout<<n<<endl;
 }
 
 int main() {
 #ifdef INSANE
    clock_t tStart = clock();
    freopen("int.txt", "r", stdin);
-   freopen("anik.txt", "w", stdout);
+   freopen("out.txt", "w", stdout);
 #endif
   
     FASTERIO; // comment when use scanf and printf 
 
    int tt; 
-   // cin>>tt; 
-   tt=1;
+   cin>>tt; 
    while(tt--)
     solve();
 
