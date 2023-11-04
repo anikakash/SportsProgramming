dca57e967e115bc496e60b345213ca083666651d
anikakash <anik15-14210@diu.edu.bd>
2023-11-05 01:30:59 +0600

    leet code daily -4

---
+++ LeetCode/1503. Last Moment Before All Ants Fall Out of a Plank.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace    std;
+
+#define flush                    cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define FASTERIO                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                       cout<<'\n';
+#define pi                       acos(-1.0) //3.1415926535897932384626
+#define pb                       push_back
+#define mk                       make_pair
+#define MaxN                     1000000007 //1e5
+#define EPS                      1e-18
+#define dpoint(x)                fixed<<setprecision(x)
+#define Fill(ar, weight)         memset(ar, weight, sizeof(ar))
+typedef long long int            ll;
+typedef double                   dl;
+typedef unsigned long long int   ull;
+
+
+int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int ans = 0;
+        for(auto it: left){
+            ans = max(ans,it);
+        }
+        for(auto it: right){
+            ans = max(ans, (n-it));
+        }
+        return ans;
+}
+
+
+int main() {
+#ifdef INSANE
+   clock_t tStart = clock();
+   freopen("int.txt", "r", stdin);  
+   freopen("out.txt", "w", stdout);
+#endif
+  
+    FASTERIO; // comment when use scanf and printf
+    
+    
+#ifdef INSANE
+   fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+    return 0;
+}
