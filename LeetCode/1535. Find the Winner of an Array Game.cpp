f4f6653c41273bc55762c755131caafb00c5a311
anikakash <anik15-14210@diu.edu.bd>
2023-11-05 16:36:11 +0600

    leet code daily - 5

---
+++ LeetCode/1535. Find the Winner of an Array Game.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace    std;
+
+#define flush                    cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define FASTERIO                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define NL                       cout<<'\n';
+#define pi                       acos(-1.0) //3.1415926535897932384626
+#define pb                       push_back
+#define mk                       make_pair
+#define MaxN                     1000000007 //1e5
+#define EPS                      1e-18
+#define dpoint(x)                fixed<<setprecision(x)
+#define Fill(ar, weight)         memset(ar, weight, sizeof(ar))
+typedef long long int            ll;
+typedef double                   dl;
+typedef unsigned long long int   ull;
+
+
+int getWinner(vector<int>& arr, int k) {
+        int cnt=0;
+        for(int i=1; i<arr.size(); i++){
+            if(arr[0]<arr[i]){
+                if(arr[0])
+                arr[0]=arr[i];
+               cnt=0;
+               cnt++;
+            }
+            else cnt++;
+
+            
+            if(cnt==k){
+                break;
+            }
+        }
+        return arr[0];
+}
+
+
+int main() {
+#ifdef INSANE
+   clock_t tStart = clock();
+   freopen("int.txt", "r", stdin);  
+   freopen("out.txt", "w", stdout);
+#endif
+  
+    FASTERIO; // comment when use scanf and printf
+    
+    
+#ifdef INSANE
+   fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+    return 0;
+}
