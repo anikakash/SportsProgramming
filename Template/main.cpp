172158210f3241bca2671c17c983455c365d1b8d
anikakash <anik15-14210@diu.edu.bd>
2024-01-26 13:55:19 +0600

    Jan-26-24

--- Template/main.cpp
+++ Template/main.cpp
@@ -15,8 +15,22 @@ typedef long long int            ll;
 typedef double                   dl;
 typedef unsigned long long int   ull;
 
-
-
+int dp[51][51][51];
+    int mod = 1e9+7;
+    int dfs(int m, int n, int maxMove, int r, int c){
+        if(r<0 || c<0 || r>=m || c>=n) return 1; // goes out of the matrix using 1 move. 
+        if(maxMove <= 0) return 0;
+        if(dp[r][c][maxMove] != -1) return dp[r][c][maxMove];
+        return dp[r][c][maxMove] = (dfs(m, n, maxMove-1, r+1, c)%mod +
+                                   dfs(m, n, maxMove-1, r-1, c)%mod + 
+                                   dfs(m, n, maxMove-1, r, c+1)%mod + 
+                                   dfs(m, n, maxMove-1, r, c-1)%mod) % mod;
+}
+    
+int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        memset(dp, -1, sizeof(dp));
+        return dfs(m, n, maxMove, startRow, startColumn);
+}
 
 int main() {
 #ifdef anikakash
@@ -27,6 +41,7 @@ int main() {
   
     FASTERIO; 
      
+ cout<<findPaths(8,50,23,5,26)<<endl;
 
 #ifdef anikakash
    fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
