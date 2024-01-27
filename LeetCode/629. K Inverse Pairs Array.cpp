e821ef6e92a59e7522c570f29213601dd6e08014
anikakash <anik15-14210@diu.edu.bd>
2024-01-28 01:58:24 +0600

    Updated with optimized code and TC explain

--- LeetCode/629. K Inverse Pairs Array.cpp
+++ LeetCode/629. K Inverse Pairs Array.cpp
@@ -1,4 +1,7 @@
-// TLE Code: 
+
+// Explain : https://youtu.be/y9yo1kyW7Bg?si=dfkQRCFMOcomEGZ9
+
+// TLE Code:  TC(K^n);
 class Solution {
 public:
     int mod = 1e9+7;
@@ -17,7 +20,7 @@ public:
 };
 
 
-// Same Code Using Memorization :  Got AC:
+// Same Code Using Memorization :  Got AC: TC : O(n*k*n) SC : O(N*K)
 class Solution {
 public:
     int mod = 1e9+7;
@@ -35,5 +38,52 @@ public:
     int kInversePairs(int n, int k) {
         memset(dp, -1, sizeof(dp));
         return solve(n,k);
+    }
+};
+
+
+// Bottom Up Solve TC  :
+class Solution {
+public:
+    int mod = 1e9+7;
+    int kInversePairs(int n, int k) {
+       vector<vector<int>>dp(n+1, vector<int>(k+1));
+       for(int i=0; i<=n; i++) dp[i][0] = 1;
+
+       for(int i=1; i<=n; i++){
+           for(int j=1; j<=k; j++){
+               for(int inv = 0; inv<=min(i-1, j); inv++){
+                   dp[i][j] = (dp[i][j]%mod + dp[i-1][j-inv]%mod)%mod;
+               }
+           }
+       }
+       return dp[n][k];
+    }
+};
+
+
+// Optimized Bottom Up 
+
+class Solution {
+public:
+    int mod = 1e9+7;
+    int kInversePairs(int n, int k) {
+       vector<vector<int>>dp(n+1, vector<int>(k+1));
+       for(int i=0; i<=n; i++) dp[i][0] = 1;
+
+       for(int i=1; i<=n; i++){
+           long long int sum = 1;
+           for(int j=1; j<=k; j++){
+
+               sum+=dp[i-1][j];
+               if(j>=i) {
+                   sum -= dp[i-1][j-i];
+               }
+
+               dp[i][j] = sum%mod;
+
+           }
+       }
+       return dp[n][k];
     }
 };
