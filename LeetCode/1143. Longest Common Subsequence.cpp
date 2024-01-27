29bdbdd291dddd3dba981eed3bd8416c87332027
anikakash <anik15-14210@diu.edu.bd>
2024-01-28 00:48:47 +0600

    add

--- LeetCode/1143. Longest Common Subsequence.cpp
+++ LeetCode/1143. Longest Common Subsequence.cpp
@@ -1,4 +1,4 @@
-// TLE : 
+// TLE : TC O(2^n*m)
 class Solution {
 public:
     int lcs(string &s1, string &s2, int i, int j){
@@ -11,4 +11,19 @@ public:
     }
 };
 
-// 
+// Memorization Approch using Previous Solutations: TC : O(m*n)
+// To Down approch DP: 
+
+class Solution {
+public:
+    int lcs(string &s1, string &s2, int i, int j, vector<vector<int>>&dp){
+        if(i == s1.size() || j == s2.size()) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1[i] == s2[j]) return dp[i][j] =  1+lcs(s1, s2, i+1, j+1, dp);
+        else return dp[i][j] = max(lcs(s1, s2, i+1, j, dp), lcs(s1, s2, i, j+1, dp));
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>>dp(text1.size(), vector<int>(text2.size(), -1));
+        return lcs(text1, text2, 0,0, dp);
+    }
+};
