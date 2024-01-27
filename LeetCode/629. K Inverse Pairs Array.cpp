21c6b00764a8b0e2b6c796fd266b104e3fbefdda
anikakash <anik15-14210@diu.edu.bd>
2024-01-28 00:51:53 +0600

    Jan-28-24

--- LeetCode/629. K Inverse Pairs Array.cpp
+++ LeetCode/629. K Inverse Pairs Array.cpp
@@ -15,3 +15,25 @@ public:
         return solve(n,k);
     }
 };
+
+
+// Same Code Using Memorization :  Got AC:
+class Solution {
+public:
+    int mod = 1e9+7;
+    int dp[1001][1001];
+    int solve(int n, int k){
+        if(n==0) return 0;
+        if(k==0) return 1;
+        if(dp[n][k] != -1) return dp[n][k];
+        int res = 0;
+        for(int i=0; i<=min(n-1,k); i++){
+            res = (res + solve(n-1, k-i)%mod)%mod;
+        }
+        return dp[n][k] = res;
+    }
+    int kInversePairs(int n, int k) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n,k);
+    }
+};
